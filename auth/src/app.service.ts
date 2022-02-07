a733afdf2d76a8d2d92de94f8a34e366dfcb7534
bugrahanozturkk <70200551+bugrahanozturkk@users.noreply.github.com>
2022-02-07 15:58:51 +0300

    Add the getUser endpoint

--- auth/src/app.service.ts
+++ auth/src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { GetUserRequest } from './get-user-request.dto';
 
 @Injectable()
 export class AppService {
+  private readonly users: any[] = [
+    {
+      userid: '123',
+      stripeUserId: '123123',
+    },
+    {
+      userid: '324',
+      stripeUserId: '324234',
+    },
+  ];
   getHello(): string {
     return 'Hello World!';
+  }
+  getUser(GetUserRequest: GetUserRequest) {
+    return this.users.find((user) => [user.userid === GetUserRequest.userId]);
   }
 }
